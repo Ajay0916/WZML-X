6575d041fa14f1c69614e08b02b1d1056582a302
Ajay Choudhary <94453305+Ajay0916@users.noreply.github.com>
2024-09-07 10:42:15 +0530

    Update images.py
    Signed-off-by: Ajay Choudhary <94453305+Ajay0916@users.noreply.github.com>

--- bot/modules/images.py
+++ bot/modules/images.py
@@ -138,4 +138,3 @@ async def pics_callback(_, query):
 bot.add_handler(MessageHandler(picture_add, filters=command(BotCommands.AddImageCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
 bot.add_handler(MessageHandler(pictures, filters=command(BotCommands.ImagesCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
 bot.add_handler(CallbackQueryHandler(pics_callback, filters=regex(r'^images')))
-        
