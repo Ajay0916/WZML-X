d4e0ed0ef0e4317a55e90ec164c6c16bf72c2182
Ajay Choudhary <94453305+Ajay0916@users.noreply.github.com>
2024-09-08 15:40:03 +0530

    Update images.py
    Signed-off-by: Ajay Choudhary <94453305+Ajay0916@users.noreply.github.com>

--- bot/modules/images.py
+++ bot/modules/images.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python3
-from asyncio import sleep as asleep
-from aiofiles.os import path as aiopath, remove as aioremove, mkdir
 from mega import MegaApi
-
+import asyncio
+from aiofiles.os import path as aiopath, remove as aioremove, mkdir
+from telegraph import upload_file
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 from pyrogram.filters import command, regex
-
 from bot import bot, LOGGER, config_dict, DATABASE_URL
 from bot.helper.telegram_helper.message_utils import sendMessage, editMessage, deleteMessage
 from bot.helper.ext_utils.bot_utils import handleIndex, new_task
@@ -14,29 +12,28 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.db_handler import DbManger
 from bot.helper.telegram_helper.button_build import ButtonMaker
 
-# Initialize Mega instance
-mega = Mega()
+# Initialize MegaApi instance
+mega = MegaApi()
 
 @new_task
 async def picture_add(_, message):
     resm = message.reply_to_message
     editable = await sendMessage(message, "<i>Fetching Input ...</i>")
     
-    # Handle Mega.nz folder URL
     if len(message.command) > 1 or resm and resm.text:
         msg_text = resm.text if resm else message.command[1]
         if msg_text.startswith("https://mega.nz/folder/"):
             folder_link = msg_text.strip()
             await editMessage(editable, f"<b>Processing Mega.nz Folder:</b> <code>{folder_link}</code>")
             try:
-                # Login to Mega (public folders still require login for API access)
-                m = mega.login()
-                folder = m.get_folder(folder_link)
+                # Use MegaApi to access the folder
+                mega.login()  # Login if required
+                folder = mega.get_folder_contents(folder_link)
                 
                 # Extract and add image links
-                for file in folder:
-                    if file['type'] == 'file' and file['name'].lower().endswith(('.jpg', '.jpeg', '.png')):
-                        file_url = file['url']
+                for item in folder:
+                    if item['type'] == 'file' and item['name'].lower().endswith(('.jpg', '.jpeg', '.png')):
+                        file_url = mega.get_url(item['hash'])
                         pic_add = file_url
                         config_dict['IMAGES'].append(pic_add)
                 
@@ -49,7 +46,6 @@ async def picture_add(_, message):
                 LOGGER.error(f"Mega.nz Folder Error: {str(e)}")
                 await editMessage(editable, str(e))
     
-    # Handle other image addition methods
     elif resm and resm.photo:
         if resm.photo.file_size > 5242880 * 2:
             return await editMessage(editable, "<i>Media is Not Supported! Only Photos!!</i>")
@@ -73,7 +69,7 @@ async def picture_add(_, message):
     if DATABASE_URL:
         await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
 
-    await asleep(1.5)
+    await asyncio.sleep(1.5)
     await editMessage(editable, f"<b><i>Successfully Added to Images List!</i></b>\n\n<b>• Total Images : {len(config_dict['IMAGES'])}</b>")
 
 async def pictures(_, message):
@@ -147,4 +143,3 @@ async def pics_callback(_, query):
 bot.add_handler(MessageHandler(picture_add, filters=command(BotCommands.AddImageCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
 bot.add_handler(MessageHandler(pictures, filters=command(BotCommands.ImagesCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
 bot.add_handler(CallbackQueryHandler(pics_callback, filters=regex(r'^images')))
-                        
