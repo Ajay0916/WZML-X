0489b2595fe3ad17371661d50470411e322fa4f6
Ajay Choudhary <94453305+Ajay0916@users.noreply.github.com>
2024-09-08 13:28:51 +0530

    Update images.py
    Signed-off-by: Ajay Choudhary <94453305+Ajay0916@users.noreply.github.com>

--- bot/modules/images.py
+++ bot/modules/images.py
@@ -24,7 +24,6 @@ async def get_drive_image_links(service, folder_id):
     image_links = []
     for item in items:
         file_id = item['id']
-        file_name = item['name']
         image_link = f"https://drive.google.com/uc?id={file_id}"  # Google Drive direct link format
         image_links.append(image_link)
     return image_links
@@ -113,9 +112,78 @@ async def picture_add(_, message):
         return await editMessage(editable, help_msg)
 
 
-# Other handlers and callbacks remain the same
+# Define the pictures function to display images
+async def pictures(_, message):
+    editable = await sendMessage(message, "<i>Fetching Images ...</i>")
+    
+    if not config_dict['IMAGES']:
+        return await editMessage(editable, "<b>No images have been added yet!</b>")
+
+    images_list = config_dict['IMAGES']
+    msg_text = "<b>Here are the current images:</b>\n\n"
+
+    for index, img_link in enumerate(images_list, start=1):
+        msg_text += f"<b>{index}.</b> <a href='{img_link}'>Image {index}</a>\n"
+
+    await editMessage(editable, msg_text)
 
 
+# Define the pics_callback function to handle button interactions
+@new_task
+async def pics_callback(_, query):
+    message = query.message
+    user_id = query.from_user.id
+    data = query.data.split()
+    if user_id != int(data[1]):
+        await query.answer(text="Not Authorized User!", show_alert=True)
+        return
+    if data[2] == "turn":
+        await query.answer()
+        ind = handleIndex(int(data[3]), config_dict['IMAGES'])
+        no = len(config_dict['IMAGES']) - abs(ind+1) if ind < 0 else ind + 1
+        pic_info = f'🌄 <b>Image No. : {no} / {len(config_dict["IMAGES"])}</b>'
+        buttons = ButtonMaker()
+        buttons.ibutton("<<", f"images {data[1]} turn {ind-1}")
+        buttons.ibutton(">>", f"images {data[1]} turn {ind+1}")
+        buttons.ibutton("Remove Image", f"images {data[1]} remov {ind}")
+        buttons.ibutton("Close", f"images {data[1]} close")
+        buttons.ibutton("Remove All", f"images {data[1]} removall", 'footer')
+        await editMessage(message, pic_info, buttons.build_menu(2), config_dict['IMAGES'][ind])
+    elif data[2] == "remov":
+        config_dict['IMAGES'].pop(int(data[3]))
+        if DATABASE_URL:
+            await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
+        query.answer("Image Successfully Deleted", show_alert=True)
+        if len(config_dict['IMAGES']) == 0:
+            await deleteMessage(query.message)
+            await sendMessage(message, f"<b>No Photo to Show !</b> Add by /{BotCommands.AddImageCommand}")
+            return
+        ind = int(data[3])+1
+        ind = len(config_dict['IMAGES']) - abs(ind) if ind < 0 else ind
+        pic_info = f'🌄 <b>Image No. : {ind+1} / {len(config_dict["IMAGES"])}</b>'
+        buttons = ButtonMaker()
+        buttons.ibutton("<<", f"images {data[1]} turn {ind-1}")
+        buttons.ibutton(">>", f"images {data[1]} turn {ind+1}")
+        buttons.ibutton("Remove Image", f"images {data[1]} remov {ind}")
+        buttons.ibutton("Close", f"images {data[1]} close")
+        buttons.ibutton("Remove All", f"images {data[1]} removall", 'footer')
+        await editMessage(message, pic_info, buttons.build_menu(2), config_dict['IMAGES'][ind])
+    elif data[2] == 'removall':
+        config_dict['IMAGES'].clear()
+        if DATABASE_URL:
+            await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
+        await query.answer("All Images Successfully Deleted", show_alert=True)
+        await sendMessage(message, f"<b>No Images to Show !</b> Add by /{BotCommands.AddImageCommand}")
+        await deleteMessage(message)
+    else:
+        await query.answer()
+        await deleteMessage(message)
+        if message.reply_to_message:
+            await deleteMessage(message.reply_to_message)
+
+
+# Register handlers
 bot.add_handler(MessageHandler(picture_add, filters=command(BotCommands.AddImageCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
 bot.add_handler(MessageHandler(pictures, filters=command(BotCommands.ImagesCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
 bot.add_handler(CallbackQueryHandler(pics_callback, filters=regex(r'^images')))
+        
