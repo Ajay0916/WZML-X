060a3e3fd21590c5d317f372e87256ae43f715c3
Ajay Choudhary <94453305+Ajay0916@users.noreply.github.com>
2024-09-08 14:15:35 +0530

    Update images.py
    Signed-off-by: Ajay Choudhary <94453305+Ajay0916@users.noreply.github.com>

--- bot/modules/images.py
+++ bot/modules/images.py
@@ -1,102 +1,52 @@
 #!/usr/bin/env python3
 from asyncio import sleep as asleep
-import aiohttp
-from urllib.parse import urlparse
+from aiofiles.os import path as aiopath, remove as aioremove, mkdir
+from telegraph import upload_file  # This import can be removed now
 
 from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 from pyrogram.filters import command, regex
 
 from bot import bot, LOGGER, config_dict, DATABASE_URL
 from bot.helper.telegram_helper.message_utils import sendMessage, editMessage, deleteMessage
+from bot.helper.ext_utils.bot_utils import handleIndex, new_task
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.db_handler import DbManger
 from bot.helper.telegram_helper.button_build import ButtonMaker
 
-
-# Function to get all image files from a publicly shared Google Drive folder
-async def get_drive_image_links(folder_url):
-    parsed_url = urlparse(folder_url)
-    folder_id = parsed_url.path.split('/')[3]
-
-    if not folder_id:
-        return []
-
-    folder_list_url = f"https://www.googleapis.com/drive/v3/files?q='{folder_id}' in parents and mimeType contains 'image/'&key=YOUR_API_KEY"
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(folder_list_url) as response:
-            if response.status != 200:
-                LOGGER.error(f"Failed to fetch folder page, status code: {response.status}")
-                return []
-
-            data = await response.json()
-            image_links = []
-
-            for item in data.get('files', []):
-                file_id = item['id']
-                image_links.append(f"https://drive.google.com/uc?id={file_id}")
-
-            return image_links
-
-
+@new_task
 async def picture_add(_, message):
     resm = message.reply_to_message
     editable = await sendMessage(message, "<i>Fetching Input ...</i>")
-
     if len(message.command) > 1 or resm and resm.text:
         msg_text = resm.text if resm else message.command[1]
         if not msg_text.startswith("http"):
             return await editMessage(editable, "<b>Not a Valid Link, Must Start with 'http'</b>")
-
-        if "drive.google.com" in msg_text:
-            image_links = await get_drive_image_links(msg_text)
-            if not image_links:
-                return await editMessage(editable, "<b>No images found in the Google Drive folder!</b>")
-
-            config_dict['IMAGES'].extend(image_links)
-            if DATABASE_URL:
-                await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
-
-            await asleep(1.5)
-            await editMessage(editable, f"<b><i>Successfully Added {len(image_links)} Images to the List!</i></b>\n\n<b>• Total Images : {len(config_dict['IMAGES'])}</b>")
-        else:
-            pic_add = msg_text.strip()
-            await editMessage(editable, f"<b>Adding your Link :</b> <code>{pic_add}</code>")
-            config_dict['IMAGES'].append(pic_add)
-            if DATABASE_URL:
-                await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
-            await asleep(1.5)
-            await editMessage(editable, f"<b><i>Successfully Added to Images List!</i></b>\n\n<b>• Total Images : {len(config_dict['IMAGES'])}</b>")
-
+        pic_add = msg_text.strip()
+        await editMessage(editable, f"<b>Adding your Link :</b> <code>{pic_add}</code>")
     elif resm and resm.photo:
         if resm.photo.file_size > 5242880 * 2:
             return await editMessage(editable, "<i>Media is Not Supported! Only Photos!!</i>")
         try:
-            photo_dir = await resm.download()
-            await editMessage(editable, "<b>Now, Uploading to <code>graph.org</code>, Please Wait...</b>")
-            await asleep(1)
-            pic_add = f'https://graph.org{upload_file(photo_dir)[0]}'
-            LOGGER.info(f"Telegraph Link : {pic_add}")
-            config_dict['IMAGES'].append(pic_add)
+            # Get the file ID of the photo
+            photo_file_id = resm.photo.file_id
+            pic_add = f'https://t.me/{bot.username}/{photo_file_id}'
+            LOGGER.info(f"Direct Telegram Link : {pic_add}")
         except Exception as e:
             LOGGER.error(f"Images Error: {str(e)}")
             await editMessage(editable, str(e))
-        finally:
-            await aioremove(photo_dir)
-
-        if DATABASE_URL:
-            await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
-        await asleep(1.5)
-        await editMessage(editable, f"<b><i>Successfully Added to Images List!</i></b>\n\n<b>• Total Images : {len(config_dict['IMAGES'])}</b>")
-
     else:
-        help_msg = "<b>By Replying to Link (Google Drive or DDL):</b>"
-        help_msg += f"\n<code>/{BotCommands.AddImageCommand} {{link}}</code>\n"
+        help_msg = "<b>By Replying to Link (Telegra.ph or DDL):</b>"
+        help_msg += f"\n<code>/{BotCommands.AddImageCommand}" + " {link}" + "</code>\n"
         help_msg += "\n<b>By Replying to Photo on Telegram:</b>"
-        help_msg += f"\n<code>/{BotCommands.AddImageCommand} {{photo}}</code>"
+        help_msg += f"\n<code>/{BotCommands.AddImageCommand}" + " {photo}" + "</code>"
         return await editMessage(editable, help_msg)
-
+    
+    config_dict['IMAGES'].append(pic_add)
+    if DATABASE_URL:
+        await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
+    await asleep(1.5)
+    await editMessage(editable, f"<b><i>Successfully Added to Images List!</i></b>\n\n<b>• Total Images : {len(config_dict['IMAGES'])}</b>")
 
 async def pictures(_, message):
     if not config_dict['IMAGES']:
@@ -113,7 +63,7 @@ async def pictures(_, message):
         await deleteMessage(to_edit)
         await sendMessage(message, f'🌄 <b>Image No. : 1 / {len(config_dict["IMAGES"])}</b>', buttons.build_menu(2), config_dict['IMAGES'][0])
 
-
+@new_task
 async def pics_callback(_, query):
     message = query.message
     user_id = query.from_user.id
@@ -152,16 +102,20 @@ async def pics_callback(_, query):
         buttons.ibutton("Close", f"images {data[1]} close")
         buttons.ibutton("Remove All", f"images {data[1]} removall", 'footer')
         await editMessage(message, pic_info, buttons.build_menu(2), config_dict['IMAGES'][ind])
-    elif data[2] == "removall":
+    elif data[2] == 'removall':
         config_dict['IMAGES'].clear()
         if DATABASE_URL:
             await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
-        await deleteMessage(query.message)
-        await sendMessage(message, f"<b>No Photo to Show !</b> Add by /{BotCommands.AddImageCommand}")
-    elif data[2] == "close":
-        await deleteMessage(query.message)
-
+        await query.answer("All Images Successfully Deleted", show_alert=True)
+        await sendMessage(message, f"<b>No Images to Show !</b> Add by /{BotCommands.AddImageCommand}")
+        await deleteMessage(message)
+    else:
+        await query.answer()
+        await deleteMessage(message)
+        if message.reply_to_message:
+            await deleteMessage(message.reply_to_message)
 
 bot.add_handler(MessageHandler(picture_add, filters=command(BotCommands.AddImageCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
 bot.add_handler(MessageHandler(pictures, filters=command(BotCommands.ImagesCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
 bot.add_handler(CallbackQueryHandler(pics_callback, filters=regex(r'^images')))
+    
