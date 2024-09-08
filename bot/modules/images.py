66b71e6602056bae9661db08e2314b86cf4fdc0b
Ajay Choudhary <94453305+Ajay0916@users.noreply.github.com>
2024-09-08 14:37:51 +0530

    Update images.py
    Signed-off-by: Ajay Choudhary <94453305+Ajay0916@users.noreply.github.com>

--- bot/modules/images.py
+++ bot/modules/images.py
@@ -17,12 +17,14 @@ from bot.helper.telegram_helper.button_build import ButtonMaker
 async def picture_add(_, message):
     resm = message.reply_to_message
     editable = await sendMessage(message, "<i>Fetching Input ...</i>")
+    
     if len(message.command) > 1 or resm and resm.text:
         msg_text = resm.text if resm else message.command[1]
         if not msg_text.startswith("http"):
             return await editMessage(editable, "<b>Not a Valid Link, Must Start with 'http'</b>")
         media_add = msg_text.strip()
         await editMessage(editable, f"<b>Adding your Link :</b> <code>{media_add}</code>")
+    
     elif resm:
         if resm.photo or resm.document:
             if resm.photo:
@@ -31,10 +33,13 @@ async def picture_add(_, message):
                 file_id = resm.photo.file_id
             elif resm.document:
                 file_id = resm.document.file_id
-            media_add = f'https://t.me/{bot.get_me().username}/{file_id}'
+            # Await to get the bot's username
+            bot_info = await bot.get_me()
+            media_add = f'https://t.me/{bot_info.username}/{file_id}'
             LOGGER.info(f"Telegram Media Link : {media_add}")
         else:
             return await editMessage(editable, "<i>Unsupported Media Type!</i>")
+    
     else:
         help_msg = "<b>By Replying to Link (Telegra.ph or DDL):</b>"
         help_msg += f"\n<code>/{BotCommands.AddImageCommand}" + " {link}" + "</code>\n"
@@ -42,29 +47,31 @@ async def picture_add(_, message):
         help_msg += f"\n<code>/{BotCommands.AddImageCommand}" + " {photo or file}" + "</code>"
         return await editMessage(editable, help_msg)
     
-    config_dict['MEDIA'].append(media_add)
+    # Save media to the config dictionary
+    config_dict['IMAGES'].append(media_add)
     if DATABASE_URL:
-        await DbManger().update_config({'MEDIA': config_dict['MEDIA']})
+        await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
+    
     await asleep(1.5)
-    await editMessage(editable, f"<b><i>Successfully Added to Media List!</i></b>\n\n<b>• Total Media : {len(config_dict['MEDIA'])}</b>")
+    await editMessage(editable, f"<b><i>Successfully Added to Images List!</i></b>\n\n<b>• Total Images : {len(config_dict['IMAGES'])}</b>")
 
-async def media(_, message):
-    if not config_dict['MEDIA']:
+async def images(_, message):
+    if not config_dict['IMAGES']:
         await sendMessage(message, f"<b>No Media to Show !</b> Add by /{BotCommands.AddImageCommand}")
     else:
-        to_edit = await sendMessage(message, "<i>Generating Grid of your Media...</i>")
+        to_edit = await sendMessage(message, "<i>Generating Grid of your Images...</i>")
         buttons = ButtonMaker()
         user_id = message.from_user.id
-        buttons.ibutton("<<", f"media {user_id} turn -1")
-        buttons.ibutton(">>", f"media {user_id} turn 1")
-        buttons.ibutton("Remove Media", f"media {user_id} remov 0")
-        buttons.ibutton("Close", f"media {user_id} close")
-        buttons.ibutton("Remove All", f"media {user_id} removall", 'footer')
+        buttons.ibutton("<<", f"images {user_id} turn -1")
+        buttons.ibutton(">>", f"images {user_id} turn 1")
+        buttons.ibutton("Remove Image", f"images {user_id} remov 0")
+        buttons.ibutton("Close", f"images {user_id} close")
+        buttons.ibutton("Remove All", f"images {user_id} removall", 'footer')
         await deleteMessage(to_edit)
-        await sendMessage(message, f'📁 <b>Media No. : 1 / {len(config_dict["MEDIA"])}</b>', buttons.build_menu(2), config_dict['MEDIA'][0])
+        await sendMessage(message, f'📁 <b>Image No. : 1 / {len(config_dict["IMAGES"])}</b>', buttons.build_menu(2), config_dict['IMAGES'][0])
 
 @new_task
-async def media_callback(_, query):
+async def images_callback(_, query):
     message = query.message
     user_id = query.from_user.id
     data = query.data.split()
@@ -73,41 +80,41 @@ async def media_callback(_, query):
         return
     if data[2] == "turn":
         await query.answer()
-        ind = handleIndex(int(data[3]), config_dict['MEDIA'])
-        no = len(config_dict['MEDIA']) - abs(ind+1) if ind < 0 else ind + 1
-        media_info = f'📁 <b>Media No. : {no} / {len(config_dict["MEDIA"])}</b>'
+        ind = handleIndex(int(data[3]), config_dict['IMAGES'])
+        no = len(config_dict['IMAGES']) - abs(ind+1) if ind < 0 else ind + 1
+        image_info = f'📁 <b>Image No. : {no} / {len(config_dict["IMAGES"])}</b>'
         buttons = ButtonMaker()
-        buttons.ibutton("<<", f"media {data[1]} turn {ind-1}")
-        buttons.ibutton(">>", f"media {data[1]} turn {ind+1}")
-        buttons.ibutton("Remove Media", f"media {data[1]} remov {ind}")
-        buttons.ibutton("Close", f"media {data[1]} close")
-        buttons.ibutton("Remove All", f"media {data[1]} removall", 'footer')
-        await editMessage(message, media_info, buttons.build_menu(2), config_dict['MEDIA'][ind])
+        buttons.ibutton("<<", f"images {data[1]} turn {ind-1}")
+        buttons.ibutton(">>", f"images {data[1]} turn {ind+1}")
+        buttons.ibutton("Remove Image", f"images {data[1]} remov {ind}")
+        buttons.ibutton("Close", f"images {data[1]} close")
+        buttons.ibutton("Remove All", f"images {data[1]} removall", 'footer')
+        await editMessage(message, image_info, buttons.build_menu(2), config_dict['IMAGES'][ind])
     elif data[2] == "remov":
-        config_dict['MEDIA'].pop(int(data[3]))
+        config_dict['IMAGES'].pop(int(data[3]))
         if DATABASE_URL:
-            await DbManger().update_config({'MEDIA': config_dict['MEDIA']})
-        query.answer("Media Successfully Deleted", show_alert=True)
-        if len(config_dict['MEDIA']) == 0:
+            await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
+        query.answer("Image Successfully Deleted", show_alert=True)
+        if len(config_dict['IMAGES']) == 0:
             await deleteMessage(query.message)
-            await sendMessage(message, f"<b>No Media to Show !</b> Add by /{BotCommands.AddImageCommand}")
+            await sendMessage(message, f"<b>No Images to Show !</b> Add by /{BotCommands.AddImageCommand}")
             return
         ind = int(data[3])+1
-        ind = len(config_dict['MEDIA']) - abs(ind) if ind < 0 else ind
-        media_info = f'📁 <b>Media No. : {ind+1} / {len(config_dict["MEDIA"])}</b>'
+        ind = len(config_dict['IMAGES']) - abs(ind) if ind < 0 else ind
+        image_info = f'📁 <b>Image No. : {ind+1} / {len(config_dict["IMAGES"])}</b>'
         buttons = ButtonMaker()
-        buttons.ibutton("<<", f"media {data[1]} turn {ind-1}")
-        buttons.ibutton(">>", f"media {data[1]} turn {ind+1}")
-        buttons.ibutton("Remove Media", f"media {data[1]} remov {ind}")
-        buttons.ibutton("Close", f"media {data[1]} close")
-        buttons.ibutton("Remove All", f"media {data[1]} removall", 'footer')
-        await editMessage(message, media_info, buttons.build_menu(2), config_dict['MEDIA'][ind])
+        buttons.ibutton("<<", f"images {data[1]} turn {ind-1}")
+        buttons.ibutton(">>", f"images {data[1]} turn {ind+1}")
+        buttons.ibutton("Remove Image", f"images {data[1]} remov {ind}")
+        buttons.ibutton("Close", f"images {data[1]} close")
+        buttons.ibutton("Remove All", f"images {data[1]} removall", 'footer')
+        await editMessage(message, image_info, buttons.build_menu(2), config_dict['IMAGES'][ind])
     elif data[2] == 'removall':
-        config_dict['MEDIA'].clear()
+        config_dict['IMAGES'].clear()
         if DATABASE_URL:
-            await DbManger().update_config({'MEDIA': config_dict['MEDIA']})
-        await query.answer("All Media Successfully Deleted", show_alert=True)
-        await sendMessage(message, f"<b>No Media to Show !</b> Add by /{BotCommands.AddImageCommand}")
+            await DbManger().update_config({'IMAGES': config_dict['IMAGES']})
+        await query.answer("All Images Successfully Deleted", show_alert=True)
+        await sendMessage(message, f"<b>No Images to Show !</b> Add by /{BotCommands.AddImageCommand}")
         await deleteMessage(message)
     else:
         await query.answer()
@@ -117,6 +124,5 @@ async def media_callback(_, query):
 
 # Register handlers
 bot.add_handler(MessageHandler(picture_add, filters=command(BotCommands.AddImageCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
-bot.add_handler(MessageHandler(media, filters=command(BotCommands.ImagesCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
-bot.add_handler(CallbackQueryHandler(media_callback, filters=regex(r'^media')))
-        
+bot.add_handler(MessageHandler(images, filters=command(BotCommands.ImagesCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
+bot.add_handler(CallbackQueryHandler(images_callback, filters=regex(r'^images')))
